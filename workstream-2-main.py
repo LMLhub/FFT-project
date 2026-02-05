5bfd26c31181885fd9a4c8254059dd3779d1ce5a
Colm Connaughton <colm.connaughton@zonlab.com>
2026-02-05 08:10:00 +0000

    Add skeleton main.py scripts for workstream 1 & 2

---
+++ workstream-2-main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+FFT Project - Workstream 2 Main Script
+"""
+
+def main():
+  """
+  Main function for workstream 2 of the FFT project.
+  """
+  print("Starting Workstream 2 of the FFT Project...")
+  return 0
+
+if __name__ == "__main__":
+  returncode = main()
+  exit(returncode)
