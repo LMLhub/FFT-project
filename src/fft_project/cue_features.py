71d0a25565ff71955c5ca0a263bb63130b531465
e-soysal <emilie.rosenlund@gmail.com>
2026-06-15 10:49:38 +0300

    growth rate cue features updated to reflect that fractal values are 'gamma values' which are the ergodic growth rates. Hence, dynamic is no longer needed for this feature.

--- src/fft_project/cue_features.py
+++ src/fft_project/cue_features.py
@@ -3,20 +3,17 @@ import numpy as np
 import logging
 logger = logging.getLogger(__name__)
 
-def growth_rate(g1_up, g1_down, g2_up, g2_down, dynamic):
+def growth_rate(g1_up, g1_down, g2_up, g2_down):
     #This function calculates the growth rate of the g1 given the dynamic.
-    if dynamic == "multiplicative":
-        return (np.log(g1_up) + np.log(g1_down))/2
-    elif dynamic == "additive":
-        return (g1_up + g1_down)/2
-    else:
-        raise ValueError("Invalid dynamic. Must be 'multiplicative' or 'additive'.")
+    # Fractal values g must be gamma values, such that the growth rate is 
+    # the expected value of the fractal values for both dynamics.
+    return (g1_up + g1_down)/2
 
 def expected_isoelastic_utility(g1_up, g1_down, g2_up, g2_down, wealth, dynamic, eta):
     #This function calculates the expected isoelastic utility of the g1 given the eta parameter.
     if dynamic == "multiplicative":
-        x11 = g1_up*wealth
-        x12 = g1_down*wealth
+        x11 = np.exp(g1_up) * wealth
+        x12 = np.exp(g1_down) * wealth
     elif dynamic == "additive":
         x11 = g1_up + wealth
         x12 = g1_down + wealth
