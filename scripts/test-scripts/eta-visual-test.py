b8a8a6b0bed1a27d476f5418d7527e43a220f1ef
e-soysal <emilie.rosenlund@gmail.com>
2026-06-23 11:23:05 +0300

    method for plotting accuracy against time average growth rate added

--- scripts/test-scripts/eta-visual-test.py
+++ scripts/test-scripts/eta-visual-test.py
@@ -6,6 +6,7 @@ sys.path.insert(0, str(PROJECT_ROOT / "src"))
 
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 from fft_project.cue_class import Cue
 from fft_project.decision_class import FFT
@@ -15,6 +16,9 @@ from fft_project.cue_features import avoid_worst_n_ranks, growth_rate, expected_
 from fft_project.simulation_gamble_data import simulate_gamble_data
 from fft_project.create_cues_ffts import create_cues_ffts
 from fft_project.prepare_experimental_data import prepare_experimental_data
+from fft_project.analysis_compare import plot_etas_compare, plot_accuracy_gamma_scatter
+
+
 
 from fft_project.config import read_config_file
 CONFIG = read_config_file(PROJECT_ROOT / "config.yaml")
@@ -25,7 +29,7 @@ FRACTAL_VALUES_MULTI = GAMBLE_SIMULATION_CONFIG["fractals_mul"]
 def initialise():
     create_cues_ffts()
 
-def main():
+def test():
     
     # Simulate some gamble data
     gamble_data = simulate_gamble_data(100, FRACTAL_VALUES, random_seed = 42)
@@ -65,7 +69,7 @@ def main():
     print("Accuracy fft_aw_1_pb_1_a vs gr:",experiment.accuracy("fft_aw_1_pb_1_a", "fft_gr"))
     print("Accuracy fft_pb_1_aw_1_a vs gr:",experiment.accuracy("fft_pf_1_aw_1_a", "fft_gr"))
 
-def main_2():
+def plot_gamma_match():
     gamble_data, experimental_results = prepare_experimental_data(PROJECT_ROOT / "data/all_active_phase_data.csv")
     gamble_data_additive, gamble_data_multiplicative = gamble_data
     experimental_results_additive, experimental_results_multiplicative = experimental_results
@@ -78,7 +82,7 @@ def main_2():
     #print(experimental_results_additive.head())
 
     fft_names = ["fft_gr", "fft_aw_1_a", "fft_aw_2_a", "fft_pb_1_a","fft_pb_1_aw_1_a", "fft_fs", "fft_fs_aw_1_a" ]
-    experiment_a = Experiment(id="exp2",
+    experiment_a = Experiment(id="exp2_a",
                             name="Example Experiment",
                             dynamic="additive",
                             description="An example experiment using the simulated gamble data and the example FFT.",
@@ -90,11 +94,7 @@ def main_2():
     
     fft_names.append("experiment_a")
     results_a = pd.concat([results_a, experimental_results_additive], axis=1)
-    
-    
-    import numpy as np
-    from fft_project.analysis_compare import plot_etas_compare
-    
+        
     fig, ax = plt.subplots(1,2, figsize=(10,4))
     
     etas = np.linspace(-1,5,31)
@@ -138,7 +138,105 @@ def main_2():
     
     fig.savefig("eta_accuracy.png", dpi=300, bbox_inches="tight")
 
+def plot_growth_rate_match():
+    gamble_data, experimental_results = prepare_experimental_data(PROJECT_ROOT / "data/all_active_phase_data.csv")
+    gamble_data_additive, gamble_data_multiplicative = gamble_data
+    experimental_results_additive, experimental_results_multiplicative = experimental_results
+    experimental_results_additive.reset_index()
+    
+    fig, ax = plt.subplots(1,2, figsize=(10,4))    
+    
+    fft_names = ["fft_gr", "fft_aw_1_a", "fft_aw_2_a", "fft_pb_1_a","fft_pb_1_aw_1_a", "fft_fs", "fft_fs_aw_1_a" ]
+    experiment_a = Experiment(id="exp3_a",
+                            name="Example Experiment",
+                            dynamic="additive",
+                            description="An example experiment using the simulated gamble data and the example FFT.",
+                            gamble_data=gamble_data_additive,
+                            ffts=[FFT.FFT_registry[name] for name in fft_names]
+    )
+
+    results_a = experiment_a.run_experiment(wealth_update="data", random_seed=42)
+    fft_names.append("experiment_a")
+    results_a = pd.concat([results_a, experimental_results_additive], axis=1)
+
+    #fft_names = ["fft_gr","experiment_a", "fft_aw_1_a", "fft_aw_2_a", "fft_pb_1_a","fft_pb_1_aw_1_a", "fft_fs", "fft_fs_aw_1_a" ]
+    
+    plot_accuracy_gamma_scatter(results_a, fft_names,
+                                 "fft_gr", runs=1, ax=ax[0],
+                                 title = "Additive dynamics")
+    
+    fft_names = ["fft_gr", "fft_aw_1_m", "fft_aw_2_m", "fft_pb_1_m","fft_pb_1_aw_1_m", "fft_fs", "fft_fs_aw_1_m" ]
+    experiment_m = Experiment(id="exp3_m",
+                            name="Example Experiment",
+                            dynamic="multiplicative",
+                            description="An example experiment using the simulated gamble data and the example FFT.",
+                            gamble_data=gamble_data_multiplicative,
+                            ffts=[FFT.FFT_registry[name] for name in fft_names]
+    )
+
+    results_m = experiment_m.run_experiment(wealth_update="data", random_seed=42)
+    
+    fft_names.append("experiment_m")
+    results_a = pd.concat([results_m, experimental_results_multiplicative], axis=1)
+    
+    plot_accuracy_gamma_scatter(results_a, fft_names, "fft_gr",
+                                runs=1, ax=ax[1],
+                                title = "Multiplicative dynamics")
+    
+    fig.subplots_adjust(wspace=0.4)
+    fig.savefig("growth_rate_compare.png", dpi=300, bbox_inches="tight")
+
+def plot_experiment_match():
+    gamble_data, experimental_results = prepare_experimental_data(PROJECT_ROOT / "data/all_active_phase_data.csv")
+    gamble_data_additive, gamble_data_multiplicative = gamble_data
+    experimental_results_additive, experimental_results_multiplicative = experimental_results
+    experimental_results_additive.reset_index()
+    
+    fig, ax = plt.subplots(1,2, figsize=(10,4))    
+    
+    fft_names = ["fft_gr", "fft_aw_1_a", "fft_aw_2_a", "fft_pb_1_a","fft_pb_1_aw_1_a", "fft_fs", "fft_fs_aw_1_a" ]
+    experiment_a = Experiment(id="exp4_a",
+                            name="Example Experiment",
+                            dynamic="additive",
+                            description="An example experiment using the simulated gamble data and the example FFT.",
+                            gamble_data=gamble_data_additive,
+                            ffts=[FFT.FFT_registry[name] for name in fft_names]
+    )
+
+    results_a = experiment_a.run_experiment(wealth_update="data", random_seed=42)
+    fft_names.append("experiment_a")
+    results_a = pd.concat([results_a, experimental_results_additive], axis=1)
+
+    #fft_names = ["fft_gr","experiment_a", "fft_aw_1_a", "fft_aw_2_a", "fft_pb_1_a","fft_pb_1_aw_1_a", "fft_fs", "fft_fs_aw_1_a" ]
+    
+    plot_accuracy_gamma_scatter(results_a, fft_names,
+                                 "experiment_a", runs=1, ax=ax[0],
+                                 title = "Additive dynamics")
+    
+    fft_names = ["fft_gr", "fft_aw_1_m", "fft_aw_2_m", "fft_pb_1_m","fft_pb_1_aw_1_m", "fft_fs", "fft_fs_aw_1_m" ]
+    experiment_m = Experiment(id="exp4_m",
+                            name="Example Experiment",
+                            dynamic="multiplicative",
+                            description="An example experiment using the simulated gamble data and the example FFT.",
+                            gamble_data=gamble_data_multiplicative,
+                            ffts=[FFT.FFT_registry[name] for name in fft_names]
+    )
+
+    results_m = experiment_m.run_experiment(wealth_update="data", random_seed=42)
+    
+    fft_names.append("experiment_m")
+    results_a = pd.concat([results_m, experimental_results_multiplicative], axis=1)
+    
+    plot_accuracy_gamma_scatter(results_a, fft_names, "experiment_m",
+                                runs=1, ax=ax[1],
+                                title = "Multiplicative dynamics")
+    
+    fig.subplots_adjust(wspace=0.4)
+    fig.savefig("choices_compare.png", dpi=300, bbox_inches="tight")
+
 if __name__ == "__main__":
     initialise()
-    #main()
-    main_2()
+    # test()
+    plot_gamma_match()
+    plot_growth_rate_match()
+    plot_experiment_match()
--- src/fft_project/create_cues_ffts.py
+++ src/fft_project/create_cues_ffts.py
@@ -5,7 +5,7 @@ PROJECT_ROOT = Path(__file__).resolve().parents[2]
 sys.path.insert(0, str(PROJECT_ROOT / "src"))
 
 from fft_project.cue_class import Cue
-from fft_project.cue_features import avoid_worst_n_ranks, growth_rate, expected_isoelastic_utility, signs, prefere_best_n_ranks
+from fft_project.cue_features import avoid_worst_n_ranks, growth_rate, expected_isoelastic_utility, signs, prefer_best_n_ranks
 from fft_project.config import read_config_file
 import pandas as pd
 
@@ -110,9 +110,9 @@ def create_cues():
 
     Cue(
         id          = "pb_1_a",
-        name        = "Prefere best 1 of all - additive",
+        name        = "Prefer best 1 of all - additive",
         description = "Give preference to the gamble that has the best of all fractals",
-        feature     = prefere_best_n_ranks,
+        feature     = prefer_best_n_ranks,
         type        = "boolean",
         params      = {"n": 1, "fractal_values": FRACTAL_VALUES},
         required_args = ["gamma_left_up", "gamma_left_down",
@@ -121,9 +121,9 @@ def create_cues():
 
     Cue(
         id          = "pb_1_m",
-        name        = "Prefere best 1 of all - multiplicative",
+        name        = "Prefer best 1 of all - multiplicative",
         description = "Give preference to the gamble that has the best of all fractals",
-        feature     = prefere_best_n_ranks,
+        feature     = prefer_best_n_ranks,
         type        = "boolean",
         params      = {"n": 1, "fractal_values": FRACTAL_VALUES_MULTI},
         required_args = ["gamma_left_up", "gamma_left_down",
@@ -133,38 +133,38 @@ def create_cues():
 def create_ffts():
     # This script creates FFTs and saves the FFT registry to a yaml file. It can be run once and then deleted.
     FFT(id="fft_aw_1_a",
-        name="Avoid the worst - additive",
-        description="An example FFT avoid the worst.",
+        name="Avoid the worst",
+        description="An example FFT avoid the worst - additive.",
         cues=[Cue.cue_registry["aw_1_a"]])
 
     FFT(id="fft_aw_1_m",
-        name="Avoid the worst - multiplicative",
-        description="An example FFT with avoid the worst cue.",
+        name="Avoid the worst",
+        description="An example FFT with avoid the worst cue - multiplicative.",
         cues=[Cue.cue_registry["aw_1_m"]])
     
     FFT(id="fft_gr",
         name="Growth rate maximisation",
-        description="An example FFT with the growth rate cue.",
+        description="FFT with the growth rate cue",
         cues=[Cue.cue_registry["gr"]])
 
     FFT(id="fft_eu_1_5_a",
-        name="Expected Isoelastic Utility - eta=1.5, additive",
-        description="An example FFT with the expected isoelastic utility cue.",
+        name="Expected Isoelastic Utility - eta=1.5",
+        description="An example FFT with the expected isoelastic utility cue - additive.",
         cues=[Cue.cue_registry["eu_1_5_a"]])
     
     FFT(id="fft_eu_1_5_m",
-        name="Expected Isoelastic Utility - eta=1.5, multiplicative",
-        description="An example FFT with the expected isoelastic utility cue.",
+        name="Expected Isoelastic Utility - eta=1.5",
+        description="An example FFT with the expected isoelastic utility cue - multiplicative.",
         cues=[Cue.cue_registry["eu_1_5_m"]])
     
     FFT(id="fft_aw_2_a",
-         name="Avoid the two worst - additive",
+         name="Avoid the two worst",
          description="An example FFT with the avoid the worst cue.",
          cues=[Cue.cue_registry["aw_2_a"]])
     
     FFT(id="fft_aw_2_m",
-        name="Avoid the two worst - multiplicative",
-        description="An example FFT with the avoid the worst cue.",
+        name="Avoid the two worst",
+        description="An example FFT with the avoid the worst cue - multiplicative",
         cues=[Cue.cue_registry["aw_2_m"]])  
     
     FFT(id="fft_fs",
@@ -173,61 +173,65 @@ def create_ffts():
         cues=[Cue.cue_registry["fs"]])
     
     FFT(id="fft_aw_1_fs_a",
-        name="Avoid the worst then positive fractal signs - additive",
+        name="Avoid the worst then positive fractal signs",
         description="FFT with the avoid the worst first, then choose based on positive fractal signs. Additive dynamic.",
         cues=[Cue.cue_registry["aw_1_a"], Cue.cue_registry["fs"]])
 
     FFT(id="fft_fs_aw_1_a",
-        name="Positive fractal signs then avoid the worst - additive",
+        name="Positive fractal signs then avoid the worst",
         description="FFT with positive fractal signs then the avoid the worst first. Additive dynamic.",
         cues=[ Cue.cue_registry["fs"], Cue.cue_registry["aw_1_a"]])
     
     FFT(id="fft_pb_1_a",
-        name="Prefere the best - additive",
-        description="FFT that preferes the best if present. Additive dynamic.",
+        name="Prefer the best",
+        description="FFT that prefers the best if present. Additive dynamic.",
         cues=[ Cue.cue_registry["pb_1_a"]])
    
     FFT(id="fft_aw_1_pb_1_a",
-        name="Avoid the worst then prefere the best - additive",
-        description="FFT with avoid the worst then prefere the best. Additive dynamic.",
+        name="Avoid the worst then prefer the best",
+        description="FFT with avoid the worst then prefer the best. Additive dynamic.",
         cues=[ Cue.cue_registry["aw_1_a"], Cue.cue_registry["pb_1_a"]])
     
     FFT(id="fft_pb_1_aw_1_a",
-        name="Avoid the worst then prefere the best - additive",
-        description="FFT with avoid the worst then prefere the best. Additive dynamic.",
+        name="Avoid the worst then prefer the best",
+        description="FFT with avoid the worst then prefer the best. Additive dynamic.",
         cues=[ Cue.cue_registry["pb_1_a"], Cue.cue_registry["aw_1_a"]])
 
     FFT(id="fft_pb_1_m",
-        name="Prefere the best - multiplicative",
-        description="FFT that preferes the best if present. Additive dynamic.",
+        name="Prefer the best",
+        description="FFT that prefers the best if present. Additive dynamic.",
         cues=[ Cue.cue_registry["pb_1_m"]])
    
     FFT(id="fft_aw_1_pb_1_m",
-        name="Avoid the worst then prefere the best - multip.",
-        description="FFT with avoid the worst then prefere the best. Additive dynamic.",
+        name="Avoid the worst then prefer the best",
+        description="FFT with avoid the worst then prefer the best. Additive dynamic.",
         cues=[ Cue.cue_registry["aw_1_m"], Cue.cue_registry["pb_1_m"]])
     
     FFT(id="fft_pb_1_aw_1_m",
-        name="Avoid the worst then prefere the best - multip.",
-        description="FFT with avoid the worst then prefere the best. Additive dynamic.",
+        name="Avoid the worst then prefer the best",
+        description="FFT with avoid the worst then prefer the best. Additive dynamic.",
         cues=[ Cue.cue_registry["pb_1_m"], Cue.cue_registry["aw_1_m"]])
 
     FFT(id="fft_fs_aw_1_m",
-        name="Positive fractal signs then avoid the worst - multip.",
+        name="Positive fractal signs then avoid the worst",
         description="FFT with positive fractal signs then the avoid the worst first. Additive dynamic.",
         cues=[ Cue.cue_registry["fs"], Cue.cue_registry["aw_1_m"]])
     
     
-def create_cues_ffts():
+def create_cues_ffts(filepath=None):
     create_cues()
     create_ffts()
-    Cue.save_registry("cue_registry.yaml")
-    FFT.save_registry("fft_registry.yaml")
-    print("Cues and FFTs created and registries saved.")
+
+    if filepath is None:
+        registry_dir = Path(".")
+    else:
+        registry_dir = Path(filepath)
+
+    registry_dir.mkdir(parents=True, exist_ok=True)
+
+    Cue.save_registry(registry_dir / "cue_registry.yaml")
+    FFT.save_registry(registry_dir / "fft_registry.yaml")
+    print(f"Cues and FFTs created and registries saved to {registry_dir}.")
 
 if __name__ == "__main__":
-    create_cues()
-    create_ffts()
-    Cue.save_registry("cue_registry.yaml")
-    FFT.save_registry("fft_registry.yaml")
-    print("Cues and FFTs created and registries saved.")
+    create_cues_ffts()
